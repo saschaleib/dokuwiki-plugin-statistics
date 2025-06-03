33319f603c2205621b8d0db89b4798417578b4af
Sascha Leib <sascha.leib@kolmio.com>
2025-06-03 22:29:58 +0200

    DokuHTTPClient error update

--- inc/StatisticsBrowscap.class.php
+++ inc/StatisticsBrowscap.class.php
@@ -24,10 +24,10 @@ class StatisticsBrowscap extends Browscap {
      * @return string
      */
     protected function _getRemoteData($url) {
-        $http = new DokuHTTPClient($url);
+        $http = new dokuwiki\HTTP\DokuHTTPClient();
         $file = $http->get($url);
         if(!$file)
-            throw new Exception('Your server can\'t connect to external resources. Please update the file manually.');
+            throw new Exception('Your server can’t connect to external resources. Please update the file manually.');
         return $file;
     }
 }
